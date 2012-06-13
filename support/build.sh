9847dd20ca552a5cc1850605bf2b82721f6612bf
Tony Heupel <tonyheupel@gmail.com>
2012-06-13 12:03:48 -0700

    get buffer-browserify working so docs can be saved

--- support/build.sh
+++ support/build.sh
@@ -1,4 +1,4 @@
 #!/usr/env/sh
-browserify -r ravendb -r http-browserify -o ./public/javascripts/ravendb.js
+browserify -r ravendb -r http-browserify -r buffer-browserify -o ./public/javascripts/ravendb.js
 
 cat ./support/patches.js >> ./public/javascripts/ravendb.js
--- support/patches.js
+++ support/patches.js
@@ -2,6 +2,11 @@ require.define("http-browserify", function (require, module, exports, __dirname,
   module.exports = require("/usr/local/lib/node_modules/browserify/node_modules/http-browserify/index.js")
 });
 
+require.define("buffer-browserify", function (require, module, exports, __dirname, __filename) {
+  module.exports = require("/Users/tony/Projects/http-ravendb/node_modules/buffer-browserify/index.js")
+});
+var Buffer = require('buffer-browserify').Buffer
+
 require.define("ravendb", function(require, module, exports, __dirname, __filename) {
   module.exports = require("/Users/tony/Projects/http-ravendb/node_modules/ravendb/ravendb.js");
 });
